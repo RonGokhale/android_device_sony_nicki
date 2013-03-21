438ff515ba760c0af26957f160f536efceeff792
Naomi Luis <nluis@codeaurora.org>
2013-03-21 15:06:53 -0700

    nicki: Set shader cache configuration options.
    Set the maximum shader cache size and key size that will be used
    by Open GL.
    
    Change-Id: Iab4c0e3c4b57ef424507cbfb72205e23cc8cf4ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,19 @@ USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
+
+# Enables Adreno RS driver
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
