d746a1a4e5b6289da42058ea2a507dde0dfc047d
Shkerzy <niksamnoy@gmail.com>
2013-12-28 22:53:47 +0800

    Remove this because logo for our resolution was added to sony_common
    https://github.com/CyanogenMod/android_device_sony_common/commit/00ebf4bd3c43de6f2a47b8ea9391d7559203ee5e
    
    Change-Id: Ibd8b92bd7cf53039decbe568d21b0f007b3ac328

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/sony/c1905/c1905.mk)
 
-TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
 
 ## Device identifier. This must come after all inclusions
