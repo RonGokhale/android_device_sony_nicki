9bd83d564a16dc57a775cb688a160e2fa4159640
Adrian DC <radian.dc@gmail.com>
2016-10-20 14:37:59 +0530

    nicki: Sync custombootimg with cm
    Change-Id: I7a9543fc4bdf6633685e2d6fdb5773e3a9658ae0

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,24 +1,32 @@
 LOCAL_PATH := $(call my-dir)
 
+DEVICE_ROOTDIR := device/sony/nicki/rootdir/root
+DEVICE_LOGORLE := $(DEVICE_ROOTDIR)/logo.rle
+PREBUILT_RECOVERY_RAMDISK := device/sony/nicki/recovery/ramdisk-recovery.cpio
+PREBUILT_TOYBOX := device/sony/nicki/recovery/toybox
+INITSONY := $(PRODUCT_OUT)/utilities/init_sony
+
 uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
-DEVICE_ROOTDIR := device/sony/nicki/rootdir/root
-PREBUILT_RAMDISK := device/sony/nicki/recovery/ramdisk-recovery.cpio
-PREBUILT_TOYBOX := device/sony/nicki/recovery/toybox
-INITSONY := $(PRODUCT_OUT)/utilities/init_sony
-
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(PREBUILT_RAMDISK) $(INSTALLED_RAMDISK_TARGET) $(INITSONY) $(PREBUILT_TOYBOX) $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Boot image: $@")
-
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
+		$(uncompressed_ramdisk) \
+		$(PREBUILT_RECOVERY_RAMDISK) \
+		$(INSTALLED_RAMDISK_TARGET) \
+		$(INITSONY) \
+		$(PREBUILT_TOYBOX) \
+		$(PRODUCT_OUT)/utilities/keycheck \
+		$(MKBOOTIMG) $(MINIGZIP) \
+		$(INTERNAL_BOOTIMAGE_FILES)
+	@echo "----- Making boot image ------"
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) cp -a $(PRODUCT_OUT)/root $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
 
-	$(hide) cp $(DEVICE_ROOTDIR)/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
-	$(hide) cp $(PREBUILT_RAMDISK) $(PRODUCT_OUT)/combinedroot/sbin/
+	$(hide) cp $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
+	$(hide) cp $(PREBUILT_RECOVERY_RAMDISK) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PREBUILT_TOYBOX) $(PRODUCT_OUT)/combinedroot/sbin/toybox_init
 
@@ -31,11 +39,13 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(P
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_BOOTIMAGE_TARGET)
 
+	$(call pretty,"Made boot image: $@")
+
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-	$(recovery_ramdisk) \
-	$(recovery_kernel)
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
 	$(call build-recoveryimage-target, $@)
-	@echo ----- Making recovery image ------
+	@echo "----- Making recovery image ------"
 	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
-	@echo ----- Made recovery image -------- $@
+	$(call pretty,"Made recovery image: $@")
